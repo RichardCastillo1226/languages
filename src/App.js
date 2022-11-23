92ec8ba1d4629c3b7ca449dbfce62ba8835e67dd
RichardCastillo1226 <castillo.richard1226@gmail.com>
2022-11-23 00:18:30 -0500

    added buttons

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import React,  { useState } from 'react';
+
+
+function SizeButtons(props) {
+  return (
+    <div>
+      <button onClick={() => props.icrement(props.sizing + 2)}> + </button> 
+      <button onClick={() => props.icrement(props.sizing - 2)}> - </button> 
+    </div>
+) 
+}
+
+function Text(props) {
+  return (
+   <h1 style={{fontSize:`${props.sizing}px`}}> {props.greeting} </h1>
+  )
+}
+
+function Translation(props) {
+  return (
+    <div>
+      <button onClick={() => props.lang("Good Morning")}>English</button>
+      <button onClick={() => props.lang("Buenos Dias")}>Spanish</button>
+      <button onClick={() => props.lang("早上好")}>Chinese</button>
+      <button onClick={() => props.lang("Bun Dia")}>Portuguese</button>
+      <button onClick={() => props.lang("Bonjou")}>Haitian Creole</button>
+    </div> 
+  )
+}
+
+
 
 function App() {
+  const [num, setSize] = useState(32) 
+  const [msm, setText] = useState("Good Morning")
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="morning-box">
+    <SizeButtons icrement={setSize} sizing={num}/> 
+    <Text sizing={num} greeting={msm}/> 
+    <Translation lang={setText}/> 
     </div>
-  );
+  )
 }
 
 export default App;
